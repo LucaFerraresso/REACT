5853c0d5fe7c3375d502e39df5e0092db29700b1
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-05 17:09:21 +0200

    added design files

--- 05-07-2024/switcher-app/src/App.jsx
+++ 05-07-2024/switcher-app/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Switcher from "./components/switcher/Switcher";
 
 function App() {
   return (
     <>
-      <h1> Switcher-app</h1>
+      <Switcher />
     </>
   );
 }
---
+++ 05-07-2024/switcher-app/src/components/switcher/Switcher.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import styles from "./Switcher.module.css";
+
+const Switcher = () => {
+  //lo stato iniziale
+  const [input, setInput] = useState({
+    slider: 50,
+  });
+
+  // funzione handle price, per gestire "l'onChange" dell'input nell'elemento input
+  const handlePrice = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  //calcolo il valore. converto il valore dell'input nel valore desiderato
+  //il valore dell'input lo decido io attraverso gli attributi
+  //in questo caso devido che l'input va da 0 a 100
+
+  const calcolatedPrice = () => {
+    //barra nel valore minimo
+    const min_price = 8.0;
+    //barra nel valore massimo
+    const max_price = 24.0;
+    const price = min_price + ((max_price - min_price) * input.slider) / 100;
+    //adesso lo voglio mettere a DOM
+    return price;
+  };
+
+  return (
+    <>
+      {/* start switcher */}
+      <div className={styles.switcher}>
+        {/* title and price div*/}
+        <div className={styles.title_price}>
+          <h2>100k PAGEVIEWS</h2>
+          <h1>{calcolatedPrice()}€</h1>
+          <h2>/month</h2>
+        </div>
+        {/* fine title and price div */}
+        {/* start range and button div */}
+        <div className={styles.range}>
+          <input
+            type="range"
+            name="slider"
+            min="0"
+            max="100"
+            onChange={handlePrice}
+          />
+          <button>Monthly Billing</button>
+        </div>
+        {/* fine startbrange and button div*/}
+        {/* start list and button */}
+        <div className={styles.list_button}>
+          {/* start list */}
+          <div className={styles.list}>
+            <ul>
+              <li>Unlimited websites</li>
+              <li>100% data ownership</li>
+              <li>Email reports</li>
+            </ul>
+          </div>
+          {/* fine list */}
+          {/* start button  div*/}
+          <div className={styles.button}>
+            <button>Start my trial</button>
+          </div>
+          {/* fine button div */}
+        </div>
+        {/* fine start and button div */}
+      </div>
+      {/* fine switcher */}
+    </>
+  );
+};
+
+export default Switcher;
