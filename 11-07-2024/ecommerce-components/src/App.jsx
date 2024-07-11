c63d31be3ec0d0c5ff684d78241a1e360f9356b7
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-11 16:56:59 +0200

    set font and colors

--- 11-07-2024/ecommerce-components/src/App.jsx
+++ 11-07-2024/ecommerce-components/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.module.css";
+import Button from "./components/button/Buttons";
 
 function App() {
   return (
     <>
       <h1>ciao mondo</h1>
+      <Button>Click me</Button>
     </>
   );
 }
