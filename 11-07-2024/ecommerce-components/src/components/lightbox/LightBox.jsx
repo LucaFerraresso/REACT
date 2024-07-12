2553e6458e958f91c16d90c33a364f3269daddc0
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-12 09:24:21 +0200

    feat:add lightBox logic

--- 11-07-2024/ecommerce-components/src/components/lightbox/LightBox.jsx
+++ 11-07-2024/ecommerce-components/src/components/lightbox/LightBox.jsx
@@ -2,12 +2,52 @@ import styles from "./LightBox.module.css";
 import classNames from "classnames";
 import CoverProduct from "../cover-product/CoverProduct";
 import Thumbnail from "../thumbnail/Thumbnail";
+import { useState } from "react";
+
+const DATA = [
+  {
+    id: self.crypto.randomUUID(),
+    product: "/public/images/image-product-1.jpg",
+    thumbnail: "/public/images/image-product-1-thumbnail.jpg",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    product: "/public/images/image-product-2.jpg",
+    thumbnail: "/public/images/image-product-2-thumbnail.jpg",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    product: "/public/images/image-product-3.jpg",
+    thumbnail: "/public/images/image-product-3-thumbnail.jpg",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    product: "/public/images/image-product-4.jpg",
+    thumbnail: "/public/images/image-product-4-thumbnail.jpg",
+  },
+];
 
 const LightBox = (props) => {
+  const initialiValue = DATA[0];
+  const [selectedProduct, setSelectedProduct] = useState(initialiValue);
+  const handleClick = (e) => {
+    const thumbnail = e.currentTarget.id;
+    const item = DATA.find((item) => item.id === thumbnail);
+    //console.log(thumbnail);
+    setSelectedProduct(item);
+  };
   return (
     <div className={classNames(styles.lightBox)}>
-      <CoverProduct imgUrl={"/public/images/image-product-1.jpg"} />
-      <Thumbnail thumbnail="/public/images/image-product-1-thumbnail.jpg" />
+      <CoverProduct imgUrl={selectedProduct.product} />
+      <ul className={classNames(styles.thumbnailList)}>
+        {DATA.map((item) => {
+          return (
+            <li key={item.id} id={item.id} onClick={handleClick}>
+              <Thumbnail thumbnail={item.thumbnail} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
--- 11-07-2024/ecommerce-components/src/components/thumbnail/Thumbnail.jsx
+++ 11-07-2024/ecommerce-components/src/components/thumbnail/Thumbnail.jsx
@@ -1,45 +1,13 @@
 import styles from "./Thumbnail.module.css";
 import classNames from "classnames";
 
-const DATA = [
-  {
-    id: self.crypto.randomUUID(),
-    product: "/public/images/image-product-1.jpg",
-    thumbnail: "/public/images/image-product-1-thumbnail.jpg",
-  },
-  {
-    id: self.crypto.randomUUID(),
-    product: "/public/images/image-product-2.jpg",
-    thumbnail: "/public/images/image-product-2-thumbnail.jpg",
-  },
-  {
-    id: self.crypto.randomUUID(),
-    product: "/public/images/image-product-3.jpg",
-    thumbnail: "/public/images/image-product-3-thumbnail.jpg",
-  },
-  {
-    id: self.crypto.randomUUID(),
-    product: "/public/images/image-product-4.jpg",
-    thumbnail: "/public/images/image-product-4-thumbnail.jpg",
-  },
-];
-
 const Thumbnail = ({ isSelected = false, thumbnail = "" }) => {
   return (
-    <div className={classNames(styles.wrapper)}>
-      {DATA.map((item) => {
-        return (
-          <div
-            className={classNames(
-              styles.thumbnail,
-              isSelected && styles.selected
-            )}
-            key={item.id}
-          >
-            <img src={item.thumbnail} key={item.id} />
-          </div>
-        );
-      })}
+    <div
+      className={classNames(styles.thumbnail, isSelected && styles.selected)}
+    >
+      <img src={thumbnail} alt="thumbnail" />
+
       <div className={classNames(isSelected && styles.over)}></div>
     </div>
   );
