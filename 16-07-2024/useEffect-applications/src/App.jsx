200d19bc21c0a0a25604c2d58825a59794d0575b
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-16 19:49:19 +0200

    feat:add use-effect logic . no css

--- 16-07-2024/useEffect-applications/src/App.jsx
+++ 16-07-2024/useEffect-applications/src/App.jsx
@@ -1,35 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.module.css";
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([]);
+  const [completed, setCompleted] = useState([]);
+  const [notCompleted, setNotCompleted] = useState([]);
+  const [isHidden, setIsHidden] = useState(true);
+  const [isHidden2, setIsHidden2] = useState(true);
+
+  const handleClick = () => {
+    //come generare un numero randomico
+    const random = Math.floor(Math.random() * 199) + 1;
+    getTodo(random);
+  };
+  //faccio una chiamata al server al primo render della pagina e stampo in console il risultato
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos").then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+
+        const completed = data.filter((item) => item.completed === true);
+        console.log("array completed", completed);
+        setCompleted(completed);
+
+        const notCompleted = data.filter((item) => item.completed === false);
+        console.log("array notCompleted", notCompleted);
+        setNotCompleted(notCompleted);
+      })
+    );
+  }, []);
+
+  //ora che al primo render ho filtrato i risultati voglio mappare i due array ottenuti,completed e non completed
+
+  //al click, faccio una chiamata per id
+  const getTodo = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  };
+  //funzione per chiamare il server utilizando l'input dell'utente come id
+  const handleChange = (e) => {
+    const id = e.target.value;
+    //setto id con il valore dell'input
+    getTodo(id);
+  };
+
+  //al rendertodo voglio visualizzare a dom il risultato degli array filtrati
+  const rendertodo = (e) => {
+    const { name } = e.target;
+    if (name === "completed") {
+      if (!isHidden) {
+        setIsHidden(true);
+      }
+      setIsHidden(false);
+    }
+    if (name === "notCompleted") {
+      if (!isHidden2) {
+        setIsHidden2(true);
+      }
+      setIsHidden2(false);
+    }
+  };
+
+  const reset = () => {
+    setIsHidden(true);
+    setIsHidden2(true);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.container}>
+        <h1>hello effect</h1>
+        <div className={styles.filterBar}>
+          <input type="text" onChange={handleChange} />
+          {/* al click, faccio una chiamata per id,estraendolo randomicamente */}
+          <button onClick={handleClick}>get todos</button>
+        </div>
+        <div className={styles.card}>
+          <h1>ID:"{todo.id}"</h1>
+          <h1>TODO:"{todo.title}"</h1>
+          {todo && <h1>COMPLETED:"{todo.completed ? "YES" : "NO"}"</h1>}
+        </div>
+
+        <hr></hr>
+        <div className={styles.list1}>
+          <div className={styles.section1}>
+            <h1>COMPLETED</h1>
+            <button name="completed" on onClick={rendertodo}>
+              completed
+            </button>
+          </div>
+
+          <ul className={isHidden ? styles.Completed : styles.hidden}>
+            {completed.map((item) => (
+              <li key={item.id}>{item.title}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.list2}>
+          <div className={styles.section2}>
+            <h1>NOT COMPLETED</h1>
+            <button name="notCompleted" onClick={rendertodo}>
+              not completed
+            </button>
+          </div>
+
+          <ul className={isHidden2 ? styles.Completed : styles.hidden}>
+            {notCompleted.map((item) => (
+              <li key={item.id}>{item.title}</li>
+            ))}
+          </ul>
+          <hr></hr>
+        </div>
+        <div className={styles.reset}>
+          <button onClick={reset}>RESET</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
