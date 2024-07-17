1123b78d0f640b530e30d0b1994d9d776bf42c2a
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-17 16:13:58 +0200

    feat:add Navbar element test

--- 17-07-2024/routing-test-ecommerce/src/App.jsx
+++ 17-07-2024/routing-test-ecommerce/src/App.jsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-600 text-center text-white text-3xl">
+      <h1 className="bg-red-600 text-center text-white text-3xl font-bold">
         Routing test and exercises
       </h1>
+      <Navbar />
     </>
   );
 }
---
+++ 17-07-2024/routing-test-ecommerce/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+const menulist = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <ul className="flex space-x-4">
+        {menulist.map((item, index) => (
+          <li key={index}>
+            <a href={item.path}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
