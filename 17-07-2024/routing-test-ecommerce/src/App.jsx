61c70d4e78d97b48a2d919451091086f350be820
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-17 19:07:24 +0200

    feat:add remove-cart-item-button and tailwind css

--- 17-07-2024/routing-test-ecommerce/src/App.jsx
+++ 17-07-2024/routing-test-ecommerce/src/App.jsx
@@ -1,61 +1,61 @@
 import { useEffect, useState } from "react";
 function App() {
   const [product, setProduct] = useState([]);
+  //al primo rendering , mi restituisce il prodotto all'indice 1
   useEffect(() => {
     fetch("https://api.escuelajs.co/api/v1/products")
       .then((response) => response.json())
       .then((json) => setProduct(json[1]));
   }, []);
-  //al primo render mi pusha il prodotto in localstorage
 
-  //ora voglio fare le stesse cose , ma non al primo rendering. ma al click del bottone
-  localStorage.removeItem("product");
+  //al click, aggiungo il prodotto al localstorage
+  //localStorage.removeItem("product");
   const handleClick = () => {
-    console.log("cliccato");
     if (product.length === 0) return;
     console.log(product);
     localStorage.setItem("product", JSON.stringify(product));
-    window.location.href = "/shop";
   };
   return (
     <>
-      <h1 className="bg-red-600 text-center text-white text-3xl font-bold 100vh">
-        Routing test and exercises
-      </h1>
-      <br />
-      <h2>Arrivo dall'API</h2>
-      <br />
       <div className="flex flex-col justify-center text-center bg-slate-400">
-        {/* DA QUI INIZIA IL DIV CONTENENTE LA CARD */}
-        <div className="max-w-sm mx-auto bg-green-400 shadow-md rounded-lg overflow-hidden">
-          <div className="bg-gray-200 p-4">
-            <h2 className="text-lg font-semibold text-gray-700">
-              ID:"{product.id}"{" "}
-            </h2>
+        <h1 className="bg-red-600 text-center text-white text-3xl font-bold 100vh">
+          Routing test and exercises
+        </h1>
+        <br />
+        <h2>Arrivo dall'API</h2>
+        <br />
+        <div className="flex flex-col justify-center text-center bg-slate-400">
+          {/* DA QUI INIZIA IL DIV CONTENENTE LA CARD */}
+          <div className="max-w-sm mx-auto bg-green-400 shadow-md rounded-lg overflow-hidden">
+            <div className="bg-gray-200 p-4">
+              <h2 className="text-lg font-semibold text-gray-700">
+                ID:"{product.id}"{" "}
+              </h2>
+            </div>
+            <div className="p-6">
+              <h2 className="text-xl font-bold mb-2 text-gray-800">
+                Title: "{product.title}"
+              </h2>
+              <h2 className="text-lg text-gray-700 mb-4">
+                Price: "{product.price}€"
+              </h2>
+              <p className="text-gray-600">
+                Description: "{product.description}"
+              </p>
+            </div>
+            <div className="p-4 bg-gray-200 text-center">
+              <button
+                onClick={handleClick}
+                className="bg-gray-800 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-gray-700 transform hover:scale-105 transition-transform duration-300 ease-in-out"
+              >
+                ADD TO CART
+              </button>
+            </div>
           </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold mb-2 text-gray-800">
-              Title: "{product.title}"
-            </h2>
-            <h2 className="text-lg text-gray-700 mb-4">
-              Prezzo: "{product.price}€"
-            </h2>
-            <p className="text-gray-600">
-              Descrizione: "{product.description}"
-            </p>
-          </div>
-          <div class="p-4 bg-gray-200 text-center">
-            <button
-              onClick={handleClick}
-              className="bg-gray-800 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-gray-700 transform hover:scale-105 transition-transform duration-300 ease-in-out"
-            >
-              Acquista Ora
-            </button>
-          </div>
+          {/* FINISCE QUI IL DIV CONTENENTE LA CARD */}
         </div>
-        {/* FINISCE QUI IL DIV CONTENENTE LA CARD */}
+        <br />
       </div>
-      <br />
     </>
   );
 }
--- 17-07-2024/routing-test-ecommerce/src/pages/Shop.jsx
+++ 17-07-2024/routing-test-ecommerce/src/pages/Shop.jsx
@@ -2,18 +2,24 @@ import { useEffect, useState } from "react";
 const Shop = () => {
   const [product, setProduct] = useState([]);
   useEffect(() => {
+    if (localStorage.getItem("product") === null) return;
     const product = JSON.parse(localStorage.getItem("product"));
     setProduct(product);
   }, []);
+
+  const remove = () => {
+    localStorage.removeItem("product");
+    setProduct([]);
+  };
   return (
-    <div className="flex flex-col justify-center items-center ">
+    <div className="flex flex-col justify-center items-center bg-blue-700 text-white">
       <br />
       <h2>"Arrivo dal LOCAL STORAGE"</h2>
       <br />
       <div className="max-w-sm mx-auto bg-blue-400 shadow-md rounded-lg overflow-hidden">
         <div className="bg-gray-200 p-4">
           <h2 className="text-lg font-semibold text-gray-700">
-            ID:"{product.id}"{" "}
+            Id:"{product.id}"
           </h2>
         </div>
         <div className="p-6">
@@ -21,13 +27,16 @@ const Shop = () => {
             Title: "{product.title}"
           </h2>
           <h2 className="text-lg text-gray-700 mb-4">
-            Prezzo: "{product.price}€"
+            Price: "{product.price}€"
           </h2>
-          <p className="text-gray-600">Descrizione: "{product.description}"</p>
+          <p className="text-gray-600">Descrition: "{product.description}"</p>
         </div>
         <div className="p-4 bg-gray-200 text-center">
-          <button className="bg-gray-800 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-gray-700 transform hover:scale-105 transition-transform duration-300 ease-in-out">
-            Acquista Ora
+          <button
+            onClick={remove}
+            className="bg-gray-800 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-gray-700 transform hover:scale-105 transition-transform duration-300 ease-in-out"
+          >
+            REMOVE
           </button>
         </div>
       </div>
