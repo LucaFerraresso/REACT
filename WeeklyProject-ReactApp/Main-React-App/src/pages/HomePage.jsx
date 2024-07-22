4225e4452e39369aa565a032ab2b1b27fbfe412c
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-22 19:52:31 +0200

    add layout- no bookclient.jsx

--- WeeklyProject-ReactApp/Main-React-App/src/pages/HomePage.jsx
+++ WeeklyProject-ReactApp/Main-React-App/src/pages/HomePage.jsx
@@ -17,61 +17,76 @@ const HomePage = () => {
     <>
       {" "}
       {/*inizio div app*/}
-      <div className="flex flex-col justify-center items-center p-[16px] bg-red-600 h-full w-full">
+      <div className="grid grid-rows-3 grid-cols-3 gap-6 justify-center items-center p-[16px] bg-red-600 h-full w-full">
         {/*inizio div 1 in app con modale*/}
-        <div className="flex flex-row justify-center items-center p-[16px] bg-gray-600 h-full w-full">
+        <div className="flex flex-row justify-between items-center p-[16px] bg-blue-700 h-full w-full">
           {" "}
           <h1
             onClick={() => setIsModalOpen(true)}
-            className="bg-blue-500 text-bold text-white p-4 rounded-md uppercase underline"
+            className="bg-blue-500 text-bold text-white p-2 rounded-md "
           >
-            WONNA SEE ALL THE TODOS?
+            GO TO ALL TODOS?
           </h1>
           <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
             {" "}
-            <h1 className="bg-blue-500  text-bold text-white p-4 rounded-md uppercase underline">
-              WONNA SEE ALL THE TODOS?
+            <h1 className="bg-blue-500  text-white p-2 rounded-md">
+              GO TO ALL TODOS?
             </h1>
           </Modal>
-          <button onClick={toPageOne}>PAGE ONE</button>
+          <button
+            onClick={toPageOne}
+            className="bg-blue-500 cursor-pointer text-white underline"
+          >
+            PAGE ONE
+          </button>
         </div>
         {/*inizio div 1 app*/}
 
         {/*inizio div 2 app con modale */}
-        <div className="flex flex-row justify-center items-center p-[16px] bg-orange-600 h-full w-full">
+        <div className="flex flex-row justify-between items-center p-[16px] bg-green-700 h-full w-full">
           {" "}
           <h1
             onClick={() => setIsModalOpen(true)}
-            className="bg-green-500  text-bold text-white p-4 rounded-md uppercase underline"
+            className="bg-green-500  text-white p-2 rounded-md "
           >
             GO TO "COMPLETED"
           </h1>
           <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
             {" "}
-            <h1 className="bg-green-500  text-bold text-white p-4 rounded-md uppercase underline">
+            <h1 className="bg-green-500  text-bold text-white p-2 rounded-md ">
               GO TO COMPLETED
             </h1>
           </Modal>
-          <button onClick={toPageTwo}>PAGE TWO</button>
+          <button
+            onClick={toPageTwo}
+            className="bg-green-500 cursor-pointer text-white underline"
+          >
+            PAGE TWO
+          </button>
         </div>
         {/*fine div 2 app*/}
 
         {/*inizio div 3 in app con modale*/}
-        <div className="flex flex-row justify-center items-center p-[16px] bg-blue-600 h-full w-full">
+        <div className="flex flex-row justify-between items-center p-[16px] bg-red-700 h-full w-full">
           {" "}
           <h1
             onClick={() => setIsModalOpen(true)}
-            className="bg-red-500  text-bold text-white p-4 rounded-md uppercase underline"
+            className="bg-red-500  text-white p-2 rounded-md "
           >
             GO TO "NOT-COMPLETED"
           </h1>
           <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
             {" "}
-            <h1 className="bg-red-500  text-bold text-white p-4 rounded-md uppercase underline">
+            <h1 className="bg-red-500  text-bold text-white p-2 rounded-md ">
               GO TO "NOT-COMPLETED"
             </h1>
           </Modal>
-          <button onClick={toPageThree}>PAGE THREE</button>
+          <button
+            onClick={toPageThree}
+            className="bg-red-500 cursor-pointer text-white underline"
+          >
+            PAGE THREE
+          </button>
         </div>
         {/*fine div 3 app*/}
       </div>
