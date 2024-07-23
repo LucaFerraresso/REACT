388cba73fded364099b705236adf227a7c92d443
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-07-23 19:49:53 +0200

    feat:add "fetch by Id" in ProductPage

--- WeeklyProject-ReactApp/Main-React-App/src/pages/ProductPage.jsx
+++ WeeklyProject-ReactApp/Main-React-App/src/pages/ProductPage.jsx
@@ -1,13 +1,44 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import getProductsList from "../DataClient/DataClient";
+import Card from "../components/atoms/Card/";
 
 const ProductPage = () => {
   const { id } = useParams();
-  console.log(id);
+  const [isloading, setIsLoading] = useState(true);
+
+  const [product, setProduct] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const data = await getProductsList();
+      //voglio filtrare l'array per id e visualizzare solo il prodotto cliccato
+      const filteredProduct = await data.filter((product) => product.id == id);
+      setProduct(filteredProduct);
+      console.log("await", filteredProduct);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  useEffect(() => {
+    console.log(product);
+  }, [product]);
+
+  if (isloading) {
+    return <h1>loading...</h1>;
+  }
 
   return (
     <>
       <h1>ProductPage: product id cliccato :{id} </h1>
+      {product.map((item) => (
+        <Card key={item.id} item={item} />
+      ))}
     </>
   );
 };
