34425fc79f582004eda92cfe7897a93edb507560
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-01 12:01:37 +0200

    feat:first commit, first Setup, (tailwind-FrontendMentor)

---
+++ 01-08-2024/All-in-One-Exercices/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-600 text-white text-2xl text-center uppercase">
+        All in one project
+      </h1>
+    </>
+  );
+}
+
+export default App;
