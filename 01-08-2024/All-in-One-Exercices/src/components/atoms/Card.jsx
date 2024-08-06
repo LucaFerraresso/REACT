3e3c6c9eda09aed5b3133c2acf95485d2fdae4b8
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-06 13:43:43 +0200

    feat:homepage tailwind,card tailwind

--- 01-08-2024/All-in-One-Exercices/src/components/atoms/Card.jsx
+++ 01-08-2024/All-in-One-Exercices/src/components/atoms/Card.jsx
@@ -22,21 +22,47 @@ const Card = ({ title, description, link, backgroundImage }) => {
               onMouseLeave={() => imageApi.start({ transform: "scale(1)" })}
             />
           </Link>
-          <div className="absolute top-0 right-0 mt-2 mr-2 bg-blue-500 text-white text-xs font-bold px-2 py-1 rounded">
+
+          <div
+            className="absolute top-0 right-0 mt-2 mr-2 bg-blue-500 text-white text-xs font-bold px-2 py-1 rounded border border-black"
+            style={{
+              textShadow:
+                "-1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black",
+            }}
+          >
             FREE
           </div>
         </div>
         <div className="p-6">
           <h1 className="text-gray-900 text-xl font-bold mb-2">{title}</h1>
           <p className="text-gray-700 text-base mb-4">{description}</p>
+
           <div className="flex items-center space-x-2 mb-4">
-            <span className=" text-blue-300 text-2x font-semibold px-2.5 py-0.5 rounded border border-black ">
+            <span
+              className="text-blue-300 text-2xl font-semibold px-2.5 py-0.5 rounded border border-black"
+              style={{
+                textShadow:
+                  "-1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black",
+              }}
+            >
               HTML
             </span>
-            <span className="text-blue-800 text-2x font-semibold px-2.5 py-0.5 rounded border border-black">
+            <span
+              className="text-blue-800 text-2xl font-semibold px-2.5 py-0.5 rounded border border-black"
+              style={{
+                textShadow:
+                  "-1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black",
+              }}
+            >
               CSS
             </span>
-            <span className=" text-pink-400 text-2x font-semibold px-2.5 py-0.5 rounded border border-black">
+            <span
+              className="text-pink-400 text-2xl font-semibold px-2.5 py-0.5 rounded border border-black"
+              style={{
+                textShadow:
+                  "-1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black",
+              }}
+            >
               JS
             </span>
           </div>
